321b3b442ed1914fd125ee43d34ee777791698de
Pilha Completa <fullpilha@gmail.com>
2023-11-10 15:33:04 -0300

    first commit

---
+++ src/test/java/com/schnee/SchneeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schnee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchneeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

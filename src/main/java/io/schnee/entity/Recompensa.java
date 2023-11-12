a14eebd43bec443166e3bb6cc0d4d22ef6e3b62a
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-11-12 13:08:41 -0300

    feat: configuracao das anotaçoes para o id das classes; uma anotaçao para definir o nome da coluna e outro para definir que sera uma valor gerado automaticamente

--- src/main/java/io/schnee/entity/Recompensa.java
+++ src/main/java/io/schnee/entity/Recompensa.java
@@ -2,7 +2,9 @@ package io.schnee.entity;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,5 +12,7 @@ import jakarta.persistence.Table;
 @Table(name="tb_recompensa")
 public class Recompensa {
 	@Id
+	@GeneratedValue
+	@Column(name="cd_recompensa")
 	private UUID id;
 }
--- src/main/java/io/schnee/entity/Reino.java
+++ src/main/java/io/schnee/entity/Reino.java
@@ -2,7 +2,9 @@ package io.schnee.entity;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,5 +12,7 @@ import jakarta.persistence.Table;
 @Table(name="tb_reino")
 public class Reino {
 	@Id
+	@GeneratedValue
+	@Column(name="cd_reino")
 	private UUID id;
 }
